fa2b8e45d0bc10f1c2af541ae1e2336ef7edb904
Saebastion Cole <99359888+sdcole@users.noreply.github.com>
2024-10-28 22:55:55 -0500

    Split up code and added soem menu logic
    Began working on menu logic and cleaned up code.

---
+++ Roullette/src/LCDUtils.cpp
@@ -0,0 +1,28 @@
+// lcdUtils.cpp
+#include "lcdUtils.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+//Clears top row of the lcd
+void clearTopRow() {
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
+}
+//Clears bottom row of the lcd
+void clearBottomRow() {
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+}
+/*  Passed string shows up on the top row of lcd
+*   const char* -- this is the pointer to the passed char[]
+*/
+void setTopText(const char* text) {
+    lcd.setCursor(0, 0);
+    lcd.print(text);
+}
+/*  Passed string shows up on the bottom row of lcd
+*   const char* -- this is the pointer to the passed char[]
+*/
+void setBottomText(const char* text) {
+    lcd.setCursor(0, 1);
+    lcd.print(text);
+}
--- Roullette/src/main.cpp
+++ Roullette/src/main.cpp
@@ -9,19 +9,35 @@
 //Imports
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-#include  <Wire.h>
+#include <lcdUtils.h>
+#include <Wire.h>
+#include <GameMode.h>
 
 //Prototypes
 void clearTopRow();
 void clearBottomRow();
 void setTopText(const char*);
 void setBottomText(const char*);
+void setColor(int, int, int);
 
+const char* currentGameModeText(GameMode);
+void incrementGameMode(GameMode &gameMode);
 
 //initialize the liquid crystal library
-LiquidCrystal_I2C lcd(0x27,  16, 2);
-const int buttonPin = 2;
+const int navButtonPin = 2;
+const int selectButtonPin = 3;
+
+const int relayPowerPin = 4;
+const int relaySwitchPin = 5;
+
+//Init buttons
 int buttonState = 0;  // variable for reading the pushbutton status
+int redPin= 6;
+int greenPin = 7;
+int bluePin = 8;
+
+bool isPlaying = 0;
+GameMode gameMode = GameMode::CLASSIC6;
 
 void setup() {
   
@@ -29,59 +45,78 @@ void setup() {
   lcd.init();
   // turn on the backlight
   lcd.backlight();
-  pinMode(buttonPin, INPUT);
+  //Setup Pins
+  pinMode(navButtonPin, INPUT);
+  pinMode(redPin,  OUTPUT);              
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  //Set Top Game Text
+  setTopText("TASER ROULLETTE ");
+  delay(1000);
+  //Sleep 1000ms before starting game
+  clearTopRow();
 }
+
 void loop() {
-  buttonState = digitalRead(buttonPin);
+
+  //Begining of the loop -- Here we need to begin menu logic.
+  setTopText("  SELECT MODE   ");
+  clearBottomRow();
+  setBottomText(currentGameModeText(gameMode));
+
+  buttonState = digitalRead(navButtonPin);
   if (buttonState == HIGH) {
-    setTopText("PRESSED        ");
+    incrementGameMode(gameMode);
+    clearBottomRow();
+    setBottomText(currentGameModeText(gameMode));
+    setColor(255, 0, 0); // Red Color
   }
   else {
     
   }
+
   //wait  for a second
   delay(1000);
+  setColor(0,  255, 0); // Green Color
   // tell the screen to write on the top row
-  setTopText("Hello!         ");
-  // tell the screen to write on the bottom  row
-  lcd.setCursor(0,1);
-  // tell the screen to write “Arduino_uno_guy”  on the bottom row
-  // you can change whats in the quotes to be what you want  it to be!
-  setBottomText("Arduino_uno_guy");
   delay(1000);
   clearTopRow();
   clearBottomRow();
   
 }
 
-//Clears top row of the lcd
-void clearTopRow() {
-  lcd.setCursor(0,0);
-  // tell the screen to write “hello, from” on the top  row
-  lcd.print("                ");
-}
 
-//Clears bottom row of the lcd
-void clearBottomRow() {
-  lcd.setCursor(0,1);
-  // tell the screen to write “hello, from” on the top  row
-  lcd.print("                ");
-}
-
-/*  Passed string shows up on the top row of lcd
-*   const char* -- this is the pointer to the passed char[]
+/*
+* currentGameModeText
+* This function gets passed the current game mode and returns the char array pointer that is needed to load into the display.
+*
 */
-void setTopText(const char* printArr) {
-  lcd.setCursor(0,0);
-  // tell the screen to write “hello, from” on the top  row
-  lcd.print(printArr);
+const char* currentGameModeText(GameMode gameMode) {
+
+  switch (gameMode) {
+    case GameMode::CLASSIC6:
+      return "   CLASSIC 6    ";
+      break;
+    case GameMode::CLASSIC10:
+      return "   CLASSIC 10   ";
+      break;
+    case GameMode::RANDOM6:
+      return "    RANDOM 6    ";
+      break;
+    case GameMode::RANDOM10:
+      return "    RANDOM 10   ";
+      break;
+  }
 }
 
-/*  Passed string shows up on the bottom row of lcd
-*   const char* -- this is the pointer to the passed char[]
-*/
-void setBottomText(const char* printArr) {
-  lcd.setCursor(0,1);
-  // tell the screen to write “hello, from” on the top  row
-  lcd.print(printArr);
+void incrementGameMode(GameMode &mode) {
+    mode = static_cast<GameMode>((static_cast<int>(mode) + 1) % static_cast<int>(4));
+}
+
+//This sets the color of the RGB light
+void setColor(int redValue, int greenValue,  int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin,  greenValue);
+  analogWrite(bluePin, blueValue);
 }
