988073e42e9b796d9a56f4855d2e76bd1667726d
Saebastion Cole <99359888+sdcole@users.noreply.github.com>
2024-11-08 15:07:10 -0600

    Simplified the Classic Gamemode Logic
    Simplified the classic game mode making it more efficient and straight forward.

--- Roullette/src/GameLogic.cpp
+++ Roullette/src/GameLogic.cpp
@@ -66,6 +66,7 @@ void GameLogic::playRandom(int probability) {
         delay(100);
         digitalWrite(relaySwitchPin, HIGH);
         delay(500);
+        clearBottomRow();
         return;
       }
       else {
@@ -80,238 +81,33 @@ void GameLogic::playRandom(int probability) {
 void GameLogic::playClassic(int probability) {
   //This will create a psuedo random seed that the numbers will be based on.
   randomSeed(analogRead(0));
-  bool validRandom = false;
-  int randomValue;
 
-  //Initial 5 to know if they hit or not
-  bool hit1 = false;
-  bool hit2 = false;
-  bool hit3 = false;
-  bool hit4 = false;
-  bool hit5 = false;
+  int randomValue = random(0,probability);
+  int currentValue = 0;
 
-  if (probability == 6) {
-    while(isGameActive) {
-      if (digitalRead(selectButtonPin) == HIGH) {
-        //This will make sure that the hit has not already been done.
-        while (!validRandom) {
-          randomValue = random(0,probability);
-          Serial.print("Random value: ");
-          Serial.print(randomValue);
-          //These conditions will check if the selection has already been made and loop until it hasn't.
-          if (randomValue == 0) {
+  while(isGameActive) {
+    if (digitalRead(selectButtonPin) == HIGH) {
+      if (currentValue == randomValue) {
             //TRIGGER LOSE RELAY
             setBottomText("    YOU LOSE    ");
             digitalWrite(relaySwitchPin, LOW);
             delay(100);
             digitalWrite(relaySwitchPin, HIGH);
             delay(500);
+            clearBottomRow();
             return;
-          }
-          else if (randomValue == 1) {
-            if (hit1) {
-              continue;
-            }
-            else {
-              hit1 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 2) {
-            if (hit2) {
-              continue;
-            }
-            else {
-              hit2 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 3) {
-            if (hit3) {
-              continue;
-            }
-            else {
-              hit3 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 4) {
-            if (hit4) {
-              continue;
-            }
-            else {
-              hit4 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 5) {
-            if (hit5) {
-              continue;
-            }
-            else {
-              hit5 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          
-        }
-        validRandom = false;
-        
       }
-
-    delay(500);
+      else {
+        currentValue++;
+        setBottomText("      SAFE      ");
+        delay(200);
+        clearBottomRow();
+      }
+    }
   }
-  }
-  else if (probability == 10){
-    //Since probability is 10 we need more bool conditions.
-    bool hit6 = false;
-    bool hit7 = false;
-    bool hit8 = false;
-    bool hit9 = false;
-    while(isGameActive) {
-      //This will make sure that the hit has not already been done.
-        while (!validRandom) {
-          randomValue = random(0,probability);
-          Serial.print("Random value: ");
-          Serial.print(randomValue);
-          //These conditions will check if the selection has already been made and loop until it hasn't.
-          if (randomValue == 0) {
-            //TRIGGER
-            delay(100);
-            setBottomText("    YOU LOSE    ");
-          }
-          else if (randomValue == 1) {
-            if (hit1) {
-              continue;
-            }
-            else {
-              hit1 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 2) {
-            if (hit2) {
-              continue;
-            }
-            else {
-              hit2 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 3) {
-            if (hit3) {
-              continue;
-            }
-            else {
-              hit3 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 4) {
-            if (hit4) {
-              continue;
-            }
-            else {
-              hit4 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 5) {
-            if (hit5) {
-              continue;
-            }
-            else {
-              hit5 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 6) {
-            if (hit6) {
-              continue;
-            }
-            else {
-              hit6 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 7) {
-            if (hit7) {
-              continue;
-            }
-            else {
-              hit7 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 8) {
-            if (hit8) {
-              continue;
-            }
-            else {
-              hit8 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
-          else if (randomValue == 9) {
-            if (hit9) {
-              continue;
-            }
-            else {
-              hit9 = true;
-              setBottomText("      SAFE      ");
-              delay(200);
-              clearBottomRow();
-              validRandom = true;
-            }
-          }
+  
 
-          
-        }
-        validRandom = false;
-
-    delay(500);
-  }
-
-  }
+  delay(500);
   return;
   
 }
